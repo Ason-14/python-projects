aee9a520d8c0c12be39318de8a69dc3df7146f9c
Ason-14 <alanson.garcia21@gmail.com>
2026-04-22 10:33:30 +1000

    initial commit

---
+++ concat.py
@@ -0,0 +1,13 @@
+# string concatenation exercise
+
+a = "Lanz G"
+print(f"My name is {a}")
+
+name = input("Name: ")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+
+madlib = f"Hello my name is {name}. I find programming very {adj} because I like to {verb1}. \
+one of my favourite things to do in my free time is {verb2}."
+print(madlib)
